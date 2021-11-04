484049885b683d7f0f1717a49da2e52514f0f4cf
Bosi Chen <chenbosi.bosi@gmail.com>
2021-11-04 18:54:27 -0400

    refactoring

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,36 +1,57 @@
 const request = require('request');
 
-let input = process.argv.slice(2);
-let url = '';
-
-const search = function(breed, toDo) {
-  url = `https://api.thecatapi.com/v1/breeds/search?q=${breed}`;
-
+const fetchBreedDescription = function(breedName, callback) {
+  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+  
   request(url, 'utf8', (error, response, body) => {
-    // console.log('Start request...')
-    // console.log('url =', url);
-    if (response && response.statusCode === 200 || response && response.statusCode === 201) {
-      if (error) {
-        console.log('error:', error);
-      }
-      if (!error) {
-        const data = JSON.parse(body); // console.log(typeof body); // => String
-        // console.log(data) // console.log(typeof data); // => Object
-        if (data.length === 0) {
-          console.log('Breed Not Found');
-        } else {
-          toDo(data);
-        }
-      }
-    } else {
-      console.log('Request failed!');
-      console.log(`statusCode: ${response && response.statusCode}`);
+    // console.log('error:', error);
+    // console.log('statusCode:', response && response.statusCode);
+    const data = JSON.parse(body);
+    let description = '';
+    if (error) {
+      description = null;
     }
+    if (!error) {
+      if (data.length === 0) {
+        description = 'Breed Not Found';
+      } else {
+        description = data[0]["description"];
+      }
+    }
+    callback(error, description);
   });
+
 };
 
-const printCatBreed = function(data) {
-  console.log(data[0]["description"]);
-};
+// const search = function(breed, toDo) {
 
-search(input, printCatBreed);
+// request(url, 'utf8', (error, response, body) => {
+//   // console.log('Start request...')
+//   // console.log('url =', url);
+//   if (response && response.statusCode === 200 || response && response.statusCode === 201) {
+//     if (error) {
+//       console.log('error:', error);
+//     }
+//     if (!error) {
+//       const data = JSON.parse(body); // console.log(typeof body); // => String
+//       // console.log(data) // console.log(typeof data); // => Object
+//       if (data.length !== 0) {
+//         toDo(data);
+//       }
+//       console.log(null);
+//     }
+//   } else {
+//     console.log(null);
+//     // console.log('Request failed!');
+//     // console.log(`statusCode: ${response && response.statusCode}`);
+//   }
+// });
+
+// };
+
+// const printCatBreed = function(data) {
+//   console.log(data[0]["description"]);
+// };
+
+
+module.exports = { fetchBreedDescription };
